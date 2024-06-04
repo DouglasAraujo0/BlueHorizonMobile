7439ff146b4aaa46ba402a15235a04b024768f49
douglas <douglas.arau@hotmail.com>
2024-06-03 23:09:40 -0300

    feat: Update screens with new headers and styling
    This commit updates the screens MaisLixo, OqueE, PontosColeta, and Voluntario with new headers and styling. The headers now display the appropriate titles for each screen, and the styling includes a dark background color, white text, and bold font. These changes improve the visual consistency and branding of the application.

--- src/screens/PontosColeta.tsx
+++ src/screens/PontosColeta.tsx
@@ -1,9 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function PontosColeta() {
   return (
     <View>
-        <Text>PontosColeta</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>PONTOS DE COLETA</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+  },
+  header: {
+    backgroundColor: '#363636',
+    padding: 10,
+    alignItems: 'center',
+    marginBottom: 30,
+    width: '100%',
+    marginTop: 35,
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
--- src/screens/Voluntario.tsx
+++ src/screens/Voluntario.tsx
@@ -1,9 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function Voluntario() {
   return (
     <View>
-        <Text>Voluntario</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>COMO VIRAR UM VOLUNTÁRIO?</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+  },
+  header: {
+    backgroundColor: '#363636',
+    padding: 10,
+    alignItems: 'center',
+    marginBottom: 30,
+    width: '100%',
+    marginTop: 35,
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
