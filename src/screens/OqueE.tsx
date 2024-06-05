e09c6308e641e51e11d2ef6c9e479d2370b60d1d
douglas <douglas.arau@hotmail.com>
2024-06-05 10:25:35 -0300

    feat: Update Voluntario screen with new design and layout
    This commit updates the Voluntario screen with a new design and layout. It includes changes to the header, description, wave image, join button, and footer. The screen now displays the appropriate header text, a description of the BlueHorizon mission, a wave image, a "Join us" button, and a home button in the footer. These changes improve the visual consistency and user experience of the screen.

--- src/screens/OqueE.tsx
+++ src/screens/OqueE.tsx
@@ -1,12 +1,60 @@
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-export default function OqueE() {
+export default function MaisLixo() {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>O QUE É A BLUEHORIZON?</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>O QUE É A BLUEHORIZON?</Text>
+        </View>
+
+        <Text style={styles.description}>
+          A BlueHorizon tem a missão de contribuir com a limpeza dos oceanos por meio de uma união coletiva.
+        </Text>
+
+        <View style={styles.wave}>
+          <Image
+            style={styles.waveImage}
+            source={require('../../assets/Onda.jpg')}
+          />
+        </View>
+
+        <Text style={[styles.subHeader, styles.textLeft]}>
+          De um lado, temos empresas engajadas na preservação dos oceanos e que querem financiar o projeto e incentivar os voluntários.
+        </Text>
+
+        <Text style={[styles.subHeader, styles.textRight]}>
+          Do outro lado, temos voluntários que trabalham na retirada de lixo dos oceanos/praias e que recebem um determinado valor para isso.
+        </Text>
+
+        <View style={styles.arrowContainer}>
+          <Image
+            style={styles.arrowImage}
+            source={require('../../assets/arrows.png')}
+          />
+          <Text style={[styles.subHeader2, styles.flexText]}>
+            Assim, ao mesmo tempo em que estamos garantindo um futuro limpo ao retirar os lixos dos oceanos, também garantimos aos voluntários o recebimento de dinheiro pelo seu esforço e cuidado.
+          </Text>
+        </View>
+
+        <TouchableOpacity style={styles.joinButton} onPress={() => navigation.navigate('Cadastro')}>
+          <Text style={styles.joinButtonText}>Junte-se a nós</Text>
+        </TouchableOpacity>
+
+        <View style={styles.footer}>
+          <TouchableOpacity style={styles.homeButton} onPress={() => navigation.navigate('Home2')}>
+            <Image
+              style={styles.homeButtonIcon}
+              source={require('../../assets/Casa.png')}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -20,13 +68,100 @@ const styles = StyleSheet.create({
     backgroundColor: '#363636',
     padding: 10,
     alignItems: 'center',
-    marginBottom: 30,
+    marginBottom: 20,
+    marginTop: 40,
     width: '100%',
-    marginTop: 35,
+    alignSelf: 'stretch',
   },
   headerText: {
     color: '#fff',
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  description: {
+    textAlign: 'center',
+    marginBottom: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  wave: {
+    width: '130%',
+    height: 70,
+    overflow: 'hidden',
+    marginBottom: 20,
+    marginLeft: -60,
+  },
+  waveImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  subHeader: {
+    marginBottom: 25,
+    fontSize: 14,
+  },
+  subHeader2: {
+    marginTop: 50,
+    marginBottom: 25,
+    fontSize: 14,
+    marginEnd: 50,
+    minWidth: 350,
+  },
+  textLeft: {
+    textAlign: 'left',
+    marginLeft: 10,
+  },
+  textRight: {
+    textAlign: 'right',
+    marginRight: 10,
+  },
+  textCenter: {
+    textAlign: 'center',
+  },
+  arrowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 15,
+    paddingHorizontal: 5,
+    position: 'relative', // allows absolute positioning within this container
+    marginStart: 50,
+  },
+  flexText: {
+    flex: 1,
+    textAlign: 'center', // centraliza o texto
+  },
+  arrowImage: {
+    width: 45,
+    height: 45,
+    position: 'absolute',
+    top: -10, // adjust as needed to position it higher
+    left: -40, // adjust as needed for horizontal positioning
+  },
+  joinButton: {
+    backgroundColor: '#1189E0',
+    padding: 15,
+    borderRadius: 30,
+    alignItems: 'center',
+    marginTop: 15,
+    marginBottom: 15,
+    marginHorizontal: 60,
+  },
+  joinButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  footer: {
+    backgroundColor: '#A4C8FF',
+    padding: 20,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  homeButton: {
+    alignItems: 'center',
+  },
+  homeButtonIcon: {
+    width: 45,
+    height: 45,
   },
 });
--- src/screens/Voluntario.tsx
+++ src/screens/Voluntario.tsx
@@ -1,12 +1,52 @@
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-export default function Voluntario() {
+const images = [
+  require('../../assets/Numero1.png'),
+  require('../../assets/Numero2.png'),
+  require('../../assets/Numero3.png'),
+  require('../../assets/Numero4.png'),
+  require('../../assets/Numero5.png'),
+];
+
+export default function MaisLixo() {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>COMO VIRAR UM VOLUNTÁRIO?</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>COMO VIRAR UM VOLUNTÁRIO?</Text>
+        </View>
+
+        <Text style={styles.description}>
+          A BlueHorizon tem a missão de contribuir com a limpeza dos oceanos por meio de uma união coletiva.
+        </Text>
+
+        <View style={styles.wave}>
+          <Image
+            style={styles.waveImage}
+            source={require('../../assets/Onda.jpg')}
+          />
+        </View>
+
+        
+
+        <TouchableOpacity style={styles.joinButton} onPress={()=> navigation.navigate('Cadastro')}>
+          <Text style={styles.joinButtonText}>Junte-se a nós</Text>
+        </TouchableOpacity>
+
+        <View style={styles.footer}>
+          <TouchableOpacity style={styles.homeButton} onPress={()=> navigation.navigate('Home2')}>
+            <Image
+              style={styles.homeButtonIcon}
+              source={require('../../assets/Casa.png')}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -20,13 +60,82 @@ const styles = StyleSheet.create({
     backgroundColor: '#363636',
     padding: 10,
     alignItems: 'center',
-    marginBottom: 30,
+    marginBottom: 20,
+    marginTop: 40,
     width: '100%',
-    marginTop: 35,
+    alignSelf: 'stretch',
   },
   headerText: {
     color: '#fff',
     fontSize: 20,
     fontWeight: 'bold',
   },
+  description: {
+    textAlign: 'center',
+    marginBottom: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  wave: {
+    width: '130%',
+    height: 70,
+    overflow: 'hidden',
+    marginBottom: 20,
+    marginLeft: -60,
+  },
+  waveImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  subHeader: {
+    textAlign: 'auto',
+    marginBottom: 20,
+    marginLeft: 10,
+    fontSize: 14,
+  },
+  listItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: '#FFF',
+    padding: 10,
+    borderRadius: 10,
+  },
+  listItemIcon: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  listItemText: {
+    fontSize: 16,
+    flex: 1,
+  },
+  joinButton: {
+    backgroundColor: '#1189E0',
+    padding: 15,
+    borderRadius: 30,
+    alignItems: 'center',
+    marginTop: 15,
+    marginBottom: 15,
+    marginHorizontal: 60,
+  },
+  joinButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  footer: {
+    backgroundColor: '#A4C8FF',
+    padding: 20,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  homeButton: {
+    alignItems: 'center',
+  },
+  homeButtonIcon: {
+    width: 45,
+    height: 45,
+  },
 });
+
